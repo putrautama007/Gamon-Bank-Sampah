646e8c4b85c95a1233d7018a0087d6d02d72ed37
putrautamna007 <putrautama30@gmail.com>
2018-10-25 07:43:01 +0700

    update Splash Screen

--- app/src/main/java/com/pau/putrautama/gamonbanksampah/activity/SplashScreenActivity.java
+++ app/src/main/java/com/pau/putrautama/gamonbanksampah/activity/SplashScreenActivity.java
@@ -12,7 +12,6 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        setContentView(R.layout.activity_splash_screen);
         startAnimation();
     }
     Thread splashThread;
